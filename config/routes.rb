99f52f7f14af998f723194c4e62a705e160fb12c
yuki0509 <hara.0509@icloud.com>
2020-08-16 00:53:09 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# == Route Map
+#
+#   Prefix Verb   URI Pattern          Controller#Action
+#     root GET    /                    sessions#new
+#    login GET    /login(.:format)     sessions#new
+#          POST   /login(.:format)     sessions#create
+#   logout DELETE /logout(.:format)    sessions#destroy
+#    users POST   /users(.:format)     users#create
+# new_user GET    /users/new(.:format) users#new
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:'sessions#new'
+  get 'login', to:'sessions#new'
+  post 'login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
+  resources :users, only: %i[new create]
 end
