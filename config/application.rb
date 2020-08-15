ec1062a5128233ef194c307c7aac02e25653e3c9
yuki0509 <hara.0509@icloud.com>
2020-08-15 22:04:49 +0900

    アセット、ルーティング、ヘルパー、テストの自動生成禁止

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,11 @@ module YoutubeStocker
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.skip_routes false
+      g.test_flamework false
+      g.helper false
+    end
   end
 end
