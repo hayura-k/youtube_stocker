1abe1fd5f693864777f3911f8e2eefb6927147a8
yuki0509 <hara.0509@icloud.com>
2020-08-16 00:53:36 +0900

    マイグレーション

--- db/migrate/20200815125449_sorcery_core.rb
+++ db/migrate/20200815125449_sorcery_core.rb
@@ -1,6 +1,7 @@
 class SorceryCore < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :username, null:false
       t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
