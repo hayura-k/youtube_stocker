3d5c8c40646ce73fe650dd3865cbe0674606b666
yuki0509 <hara.0509@icloud.com>
2020-08-18 02:06:54 +0900

    国際化

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,10 @@ module YoutubeStocker
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.generators do |g|
       g.assets false
