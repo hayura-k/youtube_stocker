da24a3ace087b18f3cebfa7f900599a322dbf6b1
yuki0509 <hara.0509@icloud.com>
2020-08-18 10:59:29 +0900

    postルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,22 @@
 # edit_password_reset GET    /password_resets/:id/edit(.:format) password_resets#edit
 #      password_reset PATCH  /password_resets/:id(.:format)      password_resets#update
 #                     PUT    /password_resets/:id(.:format)      password_resets#update
+#               posts GET    /posts(.:format)                    posts#index
+#                     POST   /posts(.:format)                    posts#create
+#            new_post GET    /posts/new(.:format)                posts#new
+#           edit_post GET    /posts/:id/edit(.:format)           posts#edit
+#                post GET    /posts/:id(.:format)                posts#show
+#                     PATCH  /posts/:id(.:format)                posts#update
+#                     PUT    /posts/:id(.:format)                posts#update
+#                     DELETE /posts/:id(.:format)                posts#destroy
+#   letter_opener_web        /letter_opener                      LetterOpenerWeb::Engine
+#
+# Routes for LetterOpenerWeb::Engine:
+# clear_letters DELETE /clear(.:format)                 letter_opener_web/letters#clear
+# delete_letter DELETE /:id(.:format)                   letter_opener_web/letters#destroy
+#       letters GET    /                                letter_opener_web/letters#index
+#        letter GET    /:id(/:style)(.:format)          letter_opener_web/letters#show
+#               GET    /:id/attachments/:file(.:format) letter_opener_web/letters#attachment
 
 Rails.application.routes.draw do
   root to:'sessions#new'
@@ -20,5 +36,6 @@ Rails.application.routes.draw do
   delete 'logout', to:'sessions#destroy'
   resources :users, only: %i[new create]
   resources :password_resets, only: %i[new create edit update]
+  resources :posts
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
