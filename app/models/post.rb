b858d6052b4bdea125f1a0898710cb35428305a2
yuki0509 <hara.0509@icloud.com>
2020-08-19 19:19:27 +0900

    テーブル同士の関連づけ

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :post_tag_relations, dependent: :destroy
+  has_many :tags, through: :post_tag_relations
 
   validates :youtube_url, presence: true, uniqueness: true 
 end
