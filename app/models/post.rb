344242acb821ef15e29b99ba42bf60ad6c68b615
yuki0509 <hara.0509@icloud.com>
2020-08-26 23:20:21 +0900

    タグ更新機能削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,14 +11,9 @@ class Post < ApplicationRecord
   def save_tag(sent_tags)
     # pluck(:カラム名)で値を配列で取得する
     current_tags = self.tags.pluck(:tagname) unless self.tags.nil?
-    old_tags = current_tags - sent_tags
     # 新しくタグの作成時に使用する変数
     new_tags = sent_tags - current_tags
     
-    old_tags.each do |old|
-      self.tags.delete Tag.find_by(tagname: old)
-    end
-
     new_tags.each do |new|
       # 新規のタグを追加
       new_post_tag = Tag.find_or_create_by(tagname: new)
