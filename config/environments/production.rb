62ec43a8721b64a9cddbb0ede14c3d11500a0b87
yuki0509 <hara.0509@icloud.com>
2020-08-27 21:42:50 +0900

    sendgridの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "youtube_stocker_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://youtube-stocker-0827.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
