4d896ca34f276e668b26440e2de3f1347414623a
yuki0509 <hara.0509@icloud.com>
2020-08-28 16:05:06 +0900

    herokuのwebサーバーをpumaに変更

--- config/puma.rb
+++ config/puma.rb
@@ -4,16 +4,20 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV["RAILS_MAX_THREADS"] || 5 )
 threads threads_count, threads_count
 
+preload_app!
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+
+rackup      DefaultRackup
+port        ENV["PORT"] || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -21,14 +25,15 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
