02f2cb3f753b37b0151a9dd5b7f6715b6fe38064
yuki0509 <hara.0509@icloud.com>
2020-09-02 11:22:27 +0900

    oauth用のcollback_urlを変更

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -160,11 +160,7 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.google.key = Rails.application.credentials.google[:client_id]
   config.google.secret = Rails.application.credentials.google[:client_secret]
-  config.google.callback_url =  if Rails.env == 'production'
-                                  'https://www.youtube-stocker.com' 
-                                else
-                                  "http://localhost:3000/oauth/callback?provider=google"
-                                end
+  config.google.callback_url = Settings.google.callback_url
   config.google.user_info_mapping = {:email => "email", :username => "name"}
   config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   #
