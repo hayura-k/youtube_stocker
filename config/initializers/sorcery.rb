6489ed29dc540b1d6d55340944965f04bfaad078
yuki0509 <hara.0509@icloud.com>
2020-09-02 10:39:37 +0900

    本番環境用のoauthのリダイレクト先を追加

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -160,7 +160,10 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.google.key = Rails.application.credentials.google[:client_id]
   config.google.secret = Rails.application.credentials.google[:client_secret]
-  config.google.callback_url = "http://localhost:3000/oauth/callback?provider=google"
+  config.google.callback_url =  if Rails.env == 'production'
+                                  'https://www.youtube-stocker.com' 
+                                else
+                                  "http://localhost:3000/oauth/callback?provider=google"
   config.google.user_info_mapping = {:email => "email", :username => "name"}
   config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   #
