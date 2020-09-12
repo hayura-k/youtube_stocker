f42df0e3bb46c6846bb68dc53276f6d543302c13
yuki0509 <hara.0509@icloud.com>
2020-09-12 18:16:08 +0900

    エラーページ表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,27 @@ class ApplicationController < ActionController::Base
   before_action :require_login
   add_flash_types :success, :info, :warning, :danger 
 
+  #エラーページ表示
+  unless Rails.env.development?
+    rescue_from Exception,                      with: :error500
+    rescue_from ActiveRecord::RecordNotFound,   with: :error404
+    rescue_from ActionController::RoutingError, with: :error404
+  end
+  
   private
 
   def not_authenticated
     redirect_to login_path, warning: 'ログインしてください'
   end
+
+  def error404(e)
+    render 'errors/error404', status: 404, format: [:html]
+  end
+  
+  def error500(e)
+    # エラー原因表示
+    logger.error [e,*e.backtrace].join("¥n")
+    render 'errors/error500', status: 500, format: [:html]
+  end
+  
 end
