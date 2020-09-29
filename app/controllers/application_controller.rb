14488cbf8472151157915795b9803c107fd84f91
yuki0509 <hara.0509@icloud.com>
2020-09-30 01:07:14 +0900

    エラーページ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :require_login
   add_flash_types :success, :info, :warning, :danger 
 
-  #エラーページ表示
+  # エラーページ表示
   unless Rails.env.development?
     rescue_from Exception,                      with: :error500
     rescue_from ActiveRecord::RecordNotFound,   with: :error404
@@ -15,13 +15,13 @@ class ApplicationController < ActionController::Base
     redirect_to login_path, warning: 'ログインしてください'
   end
 
-  def error404(e)
-    render 'errors/error404', status: 404, format: [:html]
+  def error404(_e)
+    render 'errors/error404', status: :not_found, format: [:html]
   end
   
   def error500(e)
     # エラー原因表示
-    logger.error [e,*e.backtrace].join("¥n")
-    render 'errors/error500', status: 500, format: [:html]
+    logger.error [e, *e.backtrace].join('¥n')
+    render 'errors/error500', status: :internal_server_error, format: [:html]
   end
 end
