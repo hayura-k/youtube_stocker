63eb0d3b7c3c8bb595959c1984416b3446dbfe4e
yuki0509 <hara.0509@icloud.com>
2020-10-03 15:40:01 +0900

    プレビュー機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :avatar)
   end
   
 end
